ca9206fd84a6f1da8cd101a1780fb97ff204e8df
Guilherme <guimvaladao@yahoo.com.br>
2026-07-13 20:17:25 -0300

    Coment about using uint64_t for the result of potentiation

--- cisco_cpp_essentials_1/powers_of_2/pow2.cpp
+++ cisco_cpp_essentials_1/powers_of_2/pow2.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main(void){
     int power;
-    uint64_t result = 1;
+    uint64_t result = 1;    // O tipo unsigned long no windows tem 32 bits. uint64_t de cstdint garante a utilização de 64 bits.
     
     cout << "Enter the power of two you seek: ";
     cin >> power;
