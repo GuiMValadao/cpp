47e84be89a2f682a0214dab49127d3be8b38272b
Guilherme <guimvaladao@yahoo.com.br>
2026-07-14 14:51:03 -0300

    Alterations on drawing pyramids; allows changing N to alter the amount of pyramids drawn; added coments

--- cisco_cpp_essentials_1/postcard_gizah/pcg.cpp
+++ cisco_cpp_essentials_1/postcard_gizah/pcg.cpp
@@ -1,37 +1,42 @@
+// Postcard from Gizah
 #include <iostream>
 
 using namespace std;
 int main(void){
-    int h, n, N = 3;
-    int base = 15; // Reserve total available space for each pyramid
-    bool flag;
+    int h, n, N = 3; // h is height, n is a number indicating one pyramid and N is the maximum number of pyramids
+    int base = 16; // Reserve total available space for each pyramid
 
+    // Geting pyramid height
     cout << "h = ";
     cin >> h;
+
+    // Verifying if input was appropriate
     if (h <= 2 || h >= 9) {
         cout << "Invalid height informed. Aborting execution." << endl;
         return -1;
     }
-    for (int row = 0; row < h; row++){
 
-        for (int i = 1; i < 3 * base+1; i++) {
-            if (i < base)
-                n = 0;
-            else if (i < 2*base)
-                n = 1;
-            else
-                n = 2;
+    // Starting drawing loop
+    for (int row = 0; row < h; row++){  // Each row
 
-            flag = false;
+        for (int i = 1; i < N * base+1; i++) {  // Each position in a row
+            
+            // Checks which pyramid the current position belongs to
+            for (int j = 1; j <= N; j++){
+                if (i < j * base) {
+                    n = (j - 1);
+                    break;
+                }
+            }
+            
+            // Checks the position and either draws an asterisk or leaves a blank space
             if (i == (h - row + n * base) || i == (h + row + n * base) || 
             (row == h-1 && (i > (h - row + n * base) && i < (h + row + n * base)))) {
                 cout << "*";
-                flag = true;
             }
-            if (!flag)
-                cout << " ";   
-            
-        }   
+            else
+                cout << " ";      
+        }
         cout << endl;
     }
     return 0;
