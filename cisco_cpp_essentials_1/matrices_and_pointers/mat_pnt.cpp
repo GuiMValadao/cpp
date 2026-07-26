fea69aebd14bf4e1d22366d4c38589939bc8d596
Guilherme <guimvaladao@yahoo.com.br>
2026-07-25 23:15:19 -0300

    Added example solution from course using a pointer to first element, with a coment about a bug in the example solution and the fix.

--- cisco_cpp_essentials_1/matrices_and_pointers/mat_pnt.cpp
+++ cisco_cpp_essentials_1/matrices_and_pointers/mat_pnt.cpp
@@ -24,3 +24,44 @@ int main(void) {
 	}
 	return 0;
 }
+
+
+//------------------------------------------------------
+// Example solution from the course, pointing to the first element of the matrix and using that to assign values
+//-------------------------------------------------------
+// #include <iostream>
+
+// using namespace std;
+
+// int main(void) {
+
+// 	int matrix[10][10] = { };
+
+// 	// p points to the top-left element of the matrix
+// 	// i.e. to the beginning of matrix
+// 	int *p = (int *)matrix;     // Note: is necessary to cast the matrix to int *, as otherwise it would resolve to int *[10].
+// 	for(int i = 0; i < 10; i++) {
+// 		for(int j = 0; j < 10; j++) {
+// 			// we have to find distancee between first element and [i][j] element
+// 			// it is j * 10 (as there are 10 elements in every row) + i 
+// 			// points to the elements
+
+            //  One bug in the model solution: the course uses (p + j * 10) + i, but this is only true due
+            // the simetry of the array. The correct version is (p + i * 10) + j, which extends to any size.
+            // The bug happens as it considers 10 elements per line, but each element is tied to j and not i, 
+            // all elements from the full lines would be the number of elements (10 in this case) times the amount
+            // of lines above the current line (which is i due 0-indexing and first line starts at i = 0), so
+// 			int *q = (p + i * 10) + j;
+// 			*q = (i+1) * (j+1);
+// 		}
+// 	}		
+
+// 	for(int i = 0; i < 10; i++) {
+// 		for(int j = 0; j < 10; j++) {
+// 			cout.width(4);
+// 			cout << matrix[i][j];
+// 		}
+// 		cout << endl;
+// 	}
+// 	return 0;
+// }
